2fb09afe3380bff3fa0b584bb2d0d5c3795e3114
sreevishnu025 <60707971+sreevishnu025@users.noreply.github.com>
2021-07-28 14:39:27 +0530

    Add files via upload

---
+++ components/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import NotificationBar from "./notificationBar/NotificationBar";
+import Temperature from "./Temperature";
+
+function App() {
+  return (
+    <div>
+      <NotificationBar /> <Temperature />
+    </div>
+  );
+}
+
+export default App;
