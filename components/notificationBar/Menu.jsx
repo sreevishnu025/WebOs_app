0106595cfc552a4fa3a3d21f43f4d2961a269791
sreevishnu025 <60707971+sreevishnu025@users.noreply.github.com>
2021-07-29 21:44:58 +0530

    updated menu (drop down)

--- components/notificationBar/Menu.jsx
+++ components/notificationBar/Menu.jsx
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 function Menu() {
-  return <div className="menu"></div>;
+  const [currentClass, setClass] = useState("dropdown-content");
+  function dropMenu() {
+    if (currentClass === "dropdown-content") setClass("dropdown-content show");
+    else setClass("dropdown-content");
+  }
+
+  return (
+    <div className="dropdown">
+      <button className="dropbtn">
+        <i className="fa fa-bars " name="dropDownMenu" onClick={dropMenu}></i>
+      </button>
+      <div id="myDropdown" className={currentClass}>
+        <a href="#home">Home</a>
+        <a href="#notepad">Notepad</a>
+        <a href="#youtube">Youtube</a>
+        <a href="#stores">Grocery stores</a>
+        <a href="#remainder">Remainder</a>
+      </div>
+    </div>
+  );
 }
 
 export default Menu;
