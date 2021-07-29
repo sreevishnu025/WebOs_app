da9d89e02816d9821b98478d33d839870f5c0f81
sreevishnu025 <60707971+sreevishnu025@users.noreply.github.com>
2021-07-29 21:45:50 +0530

    update percentage (added number)

--- components/notificationBar/Percentage.jsx
+++ components/notificationBar/Percentage.jsx
@@ -3,6 +3,8 @@ import React from "react";
 function Percentage() {
   return (
     <div className="percentage">
+      {" "}
+      93%
       <i class="fa fa-battery-full" aria-hidden="true"></i>
     </div>
   );
