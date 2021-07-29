f12e5a8029b9041f5056a68efb63563f379bf2cc
sreevishnu025 <60707971+sreevishnu025@users.noreply.github.com>
2021-07-29 21:47:11 +0530

    updated app (added flames)

--- components/App.jsx
+++ components/App.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 import NotificationBar from "./notificationBar/NotificationBar";
 import Temperature from "./Temperature";
+import Flame from "./Flame";
 
 function App() {
   return (
     <div>
       <NotificationBar /> <Temperature />
+      <div className="flames">
+        <Flame />
+        <Flame />
+        <Flame />
+        <Flame />
+      </div>
     </div>
   );
 }
